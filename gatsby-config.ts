97745d87f919d5730cb446b2970a6c5cbcd78c3c
jojun0 <jojun01835@gmail.com>
2023-08-18 15:07:46 +0900

    되나?

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,11 +5,8 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // You can also add new values here to query them like usual
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitle: `Portfolio`,
+    siteTitleAlt: `Portfolio`,
     siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
     siteUrl: `https://cara.lekoarts.de`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
